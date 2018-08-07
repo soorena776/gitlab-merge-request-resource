0e8cfaace54df6aa6ecb894fb6d221d044a4cd73
Javad Taheri <jtaheri@xevo.com>
2018-08-07 13:37:50 -0700

    converting  'in' command from shell to go

--- gitlabres/main.go
+++ gitlabres/main.go
@@ -9,8 +9,10 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strings"
 	"time"
 )
@@ -41,78 +43,89 @@ type MergeRequest struct {
 	Sha string `json:"sha"`
 }
 
-var requestHeader map[string]string
+var apiRequestHeader map[string]string
 var gitlabAPI string
-var payload []byte
+var payload Payload
+var gitlabHost string
+var port string
+var projectPath string
+var protocol = "https"
 var err error
 
 func main() {
 
+	cmd := filepath.Base(os.Args[0])
+	usage := func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s expects input (json payload) from stdin. It servers the %s purpuse of concourse resource type.\n", cmd, cmd)
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	var b bytes.Buffer
-
 	for scanner.Scan() {
 		b.WriteString(scanner.Text())
 	}
 
-	payload = b.Bytes()
-	if len(payload) == 0 {
-		panic("no payload is passed")
+	err = json.Unmarshal(b.Bytes(), &payload)
+	if err != nil {
+		usage()
+		panicIfErr(err)
 	}
 
-	cmd := filepath.Base(os.Args[0])
-
-	usage := func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s -cmd <Command Name> -payloadF <Payload file name>\n", filepath.Base(os.Args[0]))
-	}
+	apiRequestHeader = map[string]string{"private-token": payload.Source.PrivateToken}
+	configureSslVerification()
+	decomposeURI()
 
 	switch cmd {
 	case "check":
 		check()
+	case "in":
+		if len(os.Args[1]) == 0 {
+			panic("in command needs a destination folder argument")
+		}
+		in(os.Args[1])
+	case "out":
+		out()
 	default:
 		usage()
 		panic("unknown command")
 	}
 }
 
-func check() {
-	var jsonp Payload
-	err := json.Unmarshal(payload, &jsonp)
-	if err != nil {
-		panic(err)
-	}
+func out() {
 
-	requestHeader = map[string]string{"private-token": jsonp.Source.PrivateToken}
+}
 
-	uri := strings.TrimSpace(jsonp.Source.URI)
-	gitlabHost := ""
-	port := ""
-	projectPath := ""
-	protocol := "https"
-	latestVersion := jsonp.Version["sha"]
-	var re *regexp.Regexp
-	if strings.Contains(uri, "git@") {
-		re = regexp.MustCompile(".*git@(.*):([0-9]*\\/+)?(.*)\\.git")
-		res := re.FindStringSubmatch(uri)
-		gitlabHost = res[1]
-		port = res[2]
-		projectPath = res[3]
+func in(destFolder string) {
 
-	} else if strings.Index(uri, "http") == 0 {
-		re = regexp.MustCompile("(https?):\\/\\/([^\\/]*)\\/(.*)\\.git")
-		res := re.FindStringSubmatch(uri)
-		protocol = res[1]
-		gitlabHost = res[2]
-		projectPath = res[3]
+	if len(payload.Source.PrivateKey) != 0 {
+		rsaDir := os.ExpandEnv("$HOME/.ssh/ki")
+		panicIfErr(os.MkdirAll(rsaDir, os.ModeDir|0644))
+		panicIfErr(ioutil.WriteFile(rsaDir+"/id_rsa", []byte(payload.Source.PrivateKey), 0500))
+
+		pars := []string{"-t", "rsa", gitlabHost}
+		if len(port) != 0 {
+			pars = []string{"-t", "rsa", "-p", port, gitlabHost}
+		}
+
+		knownhost, err := exec.Command("ssh-keyscan", pars...).Output()
+		panicIfErr(err)
+
+		panicIfErr(ioutil.WriteFile(rsaDir+"/known_hosts", knownhost, 0500))
 	} else {
-		panic(fmt.Sprintf("The url protocol is not supported: %s", uri))
+		defLogin := fmt.Sprintf("default login %s password %s", payload.Source.Username, payload.Source.Password)
+		panicIfErr(ioutil.WriteFile(os.ExpandEnv("$HOME/.netrc"), []byte(defLogin), 0500))
 	}
+}
+
+func check() {
+
+	latestVersion := payload.Version["sha"]
 
 	_ = port
 
 	gitlabAPI = fmt.Sprintf("%s://%s/api/v4/projects/%s", protocol, gitlabHost, url.PathEscape(projectPath))
 
-	if jsonp.Source.NoSsl {
+	if payload.Source.NoSsl {
 		protocol = "http"
 	}
 
@@ -123,11 +136,8 @@ func check() {
 	}
 	newMRs := []*MergeRequest{}
 	var openMRs []*MergeRequest
-	resp := sendrequest("GET", fmt.Sprintf("%s/merge_requests?state=opened&order_by=updated_at", gitlabAPI), "", requestHeader)
-	err = json.Unmarshal(resp, &openMRs)
-	if err != nil {
-		panic(err)
-	}
+	resp := sendrequest("GET", fmt.Sprintf("%s/merge_requests?state=opened&order_by=updated_at", gitlabAPI), "")
+	panicIfErr(json.Unmarshal(resp, &openMRs))
 
 	for _, mr := range openMRs {
 		if len(mr.Sha) > 0 {
@@ -143,52 +153,81 @@ func check() {
 	}
 
 	result, err := json.Marshal(newMRs)
-	if err != nil {
-		panic(err)
-	}
+	panicIfErr(err)
 
 	fmt.Println(string(result))
 }
 
 func getMRLastUpdate(latestVersion string) time.Time {
-	resp := sendrequest("GET", fmt.Sprintf("%s/repository/commits/%s", gitlabAPI, latestVersion), "", requestHeader)
+	resp := sendrequest("GET", fmt.Sprintf("%s/repository/commits/%s", gitlabAPI, latestVersion), "")
 
 	var commit Commit
-	err := json.Unmarshal(resp, &commit)
-	if err != nil {
-		panic(err)
-	}
+	panicIfErr(json.Unmarshal(resp, &commit))
 
 	parsed, err := time.Parse(DateTimeLayout, commit.CommittedDate)
-	if err != nil {
-		panic(err)
-	}
+	panicIfErr(err)
 
 	return parsed.UTC()
 }
 
-func sendrequest(method, url, body string, header map[string]string) []byte {
+func sendrequest(method, url, body string) []byte {
 
 	req, err := http.NewRequest(method, url, bytes.NewBuffer([]byte(body)))
-	for k, v := range header {
+	for k, v := range apiRequestHeader {
 		req.Header.Set(k, v)
 	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
+	panicIfErr(err)
 	defer resp.Body.Close()
 
 	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
 		result, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			panic(err)
-		}
+		panicIfErr(err)
 
 		return result
 	}
 
 	panic(fmt.Sprintf("request from %s returned status code: %d(%s)", url, resp.StatusCode, resp.Status))
+}
+
+func decomposeURI() {
+	uri := strings.TrimSpace(payload.Source.URI)
+	var re *regexp.Regexp
+	if strings.Contains(uri, "git@") {
+		re = regexp.MustCompile(".*git@(.*):([0-9]*\\/+)?(.*)\\.git")
+		res := re.FindStringSubmatch(uri)
+		gitlabHost = res[1]
+		port = strings.Trim(res[2], "/")
+		projectPath = res[3]
+
+	} else if strings.Index(uri, "http") == 0 {
+		re = regexp.MustCompile("(https?):\\/\\/([^\\/]*)\\/(.*)\\.git")
+		res := re.FindStringSubmatch(uri)
+		protocol = res[1]
+		gitlabHost = res[2]
+		projectPath = res[3]
+	} else {
+		panic(fmt.Sprintf("The url protocol is not supported: %s", uri))
+	}
+}
+
+func configureSslVerification() {
+	if payload.Source.SkipSslVerification {
+		os.Setenv("GIT_SSL_NO_VERIFY", "true")
+		panicIfErr(ioutil.WriteFile(os.ExpandEnv("HOME/.curlrc"), []byte("insecure"), 0644))
+	}
+}
+
+func panicIfErr(err error) {
+	if err != nil {
+		fpcs := make([]uintptr, 1)
+		caller := runtime.FuncForPC(fpcs[0] - 1)
+		callerMethod, line := caller.FileLine(fpcs[0] - 1)
+
+		fmt.Printf("\nPanic at %s : %d \n", callerMethod, line)
+
+		panic(err)
+	}
 }
