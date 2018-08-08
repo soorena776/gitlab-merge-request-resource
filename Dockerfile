305f005562a91909516302caf6cf7b136d22b3da
Javad Taheri <jtaheri@xevo.com>
2018-08-08 12:41:19 -0700

    converting 'out' command from bash to go

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,11 @@ FROM golang:alpine as builder
 COPY gitlabres/ $GOPATH/src/gitlabres/
 WORKDIR $GOPATH/src/gitlabres
 RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix nocgo -o /app .
+RUN go test ./...
 
 FROM concourse/buildroot:git
 COPY scripts/ /opt/resource/
 COPY --from=builder /app /opt/resource/check
+COPY --from=builder /app /opt/resource/in
+COPY --from=builder /app /opt/resource/out
 
--- gitlabres/main.go
+++ gitlabres/main.go
@@ -22,7 +22,7 @@ const DateTimeLayout = "2006-01-02T15:04:05.000-07:00"
 type Payload struct {
 	Source  Psource           `json:"source"`
 	Version map[string]string `json:"version"`
-	Params  map[string]string `json:"params"`
+	Params  Params            `json:"params"`
 }
 
 type Psource struct {
@@ -33,6 +33,7 @@ type Psource struct {
 	Password            string `json:"password"`
 	NoSsl               bool   `json:"no_ssl"`
 	SkipSslVerification bool   `json:"skip_ssl_verification"`
+	ConcourseHost       string `json:"concourse_host"`
 }
 
 type Commit struct {
@@ -43,13 +44,20 @@ type MergeRequest struct {
 	Sha string `json:"sha"`
 }
 
-var apiRequestHeader map[string]string
-var gitlabAPI string
+type Params struct {
+	Repository string `json:"repository"`
+	Status     string `json:"status"`
+	BuildLabel string `json:"build_label"`
+}
+
+const defaultBuildLabel = "Concourse"
+
+var gitlabAPIbase string
 var payload Payload
 var gitlabHost string
 var port string
 var projectPath string
-var protocol = "https"
+var protocol string
 var err error
 
 func main() {
@@ -71,36 +79,108 @@ func main() {
 		panicIfErr(err)
 	}
 
-	apiRequestHeader = map[string]string{"private-token": payload.Source.PrivateToken}
+	checkRequired()
 	configureSslVerification()
 	decomposeURI()
 
+	if payload.Source.NoSsl {
+		protocol = "http"
+	} else {
+		protocol = "https"
+	}
+	gitlabAPIbase = fmt.Sprintf("%s://%s/api/v4/projects/%s/", protocol, gitlabHost, url.PathEscape(projectPath))
+
+	var result interface{}
+
 	switch cmd {
 	case "check":
-		check()
+		result = check()
 	case "in":
 		if len(os.Args[1]) == 0 {
 			panic("in command needs a destination folder argument")
 		}
-		in(os.Args[1])
+		result = in(os.Args[1])
 	case "out":
-		out()
+		if len(os.Args[1]) == 0 {
+			panic("out command needs a source folder argument")
+		}
+		result = out(os.Args[1])
 	default:
-		usage()
+
 		panic("unknown command")
+	}
+
+	output, err := json.Marshal(result)
+	panicIfErr(err)
+
+	fmt.Println(string(output))
+}
+
+func checkRequired() {
+	s := payload.Source
+	required := []string{s.PrivateToken, s.URI, s.PrivateKey, s.ConcourseHost}
+	for _, val := range required {
+		if len(val) == 0 {
+			panic(fmt.Sprintf("please specify all the required parameters"))
+		}
 	}
 }
 
-func out() {
+func out(sourceFolder string) map[string]map[string]string {
 
+	if len(payload.Params.Repository) == 0 {
+		panic("please specify a repository")
+	}
+	if len(payload.Params.Status) == 0 {
+		panic("please specify a status")
+	}
+	if len(payload.Source.ConcourseHost) == 0 {
+		panic("please specify the concourse host address. (format url:port)")
+	}
+	if len(payload.Params.BuildLabel) == 0 {
+		payload.Params.BuildLabel = defaultBuildLabel
+	}
+
+	panicIfErr(os.Chdir(sourceFolder))
+	panicIfErr(os.Chdir(payload.Params.Repository))
+
+	targetURL := fmt.Sprintf("%s/teams/%s/pipelines/%s/jobs/%s/builds/%s",
+		payload.Source.ConcourseHost,
+		url.PathEscape(os.Getenv("BUILD_TEAM_NAME")),
+		url.PathEscape(os.Getenv("BUILD_PIPELINE_NAME")),
+		url.PathEscape(os.Getenv("BUILD_JOB_NAME")),
+		url.PathEscape(os.Getenv("BUILD_NAME")))
+
+	out, err := exec.Command("git", "rev-parse", "HEAD").Output()
+	panicIfErr(err)
+	commitSHA := strings.TrimSpace(string(out))
+
+	bodyJSON, err := json.Marshal(map[string]interface{}{
+		"state":       payload.Params.Status,
+		"build_label": payload.Params.BuildLabel,
+		"target_url":  targetURL,
+	})
+	panicIfErr(err)
+
+	header := map[string]string{
+		"Content-Type": "application/json",
+	}
+
+	sendAPIRequest("POST", "statuses/"+commitSHA, bodyJSON, header)
+
+	return map[string]map[string]string{
+		"version": map[string]string{
+			"sha": fmt.Sprintf("\"%s\"", commitSHA),
+		},
+	}
 }
 
-func in(destFolder string) {
+func in(destFolder string) map[string]map[string]string {
 
 	if len(payload.Source.PrivateKey) != 0 {
-		rsaDir := os.ExpandEnv("$HOME/.ssh/ki")
-		panicIfErr(os.MkdirAll(rsaDir, os.ModeDir|0644))
-		panicIfErr(ioutil.WriteFile(rsaDir+"/id_rsa", []byte(payload.Source.PrivateKey), 0500))
+		rsaDir := os.ExpandEnv("$HOME/.ssh/")
+		panicIfErr(os.MkdirAll(rsaDir, os.ModeDir|0744))
+		panicIfErr(ioutil.WriteFile(rsaDir+"id_rsa", []byte(payload.Source.PrivateKey), 0500))
 
 		pars := []string{"-t", "rsa", gitlabHost}
 		if len(port) != 0 {
@@ -113,30 +193,36 @@ func in(destFolder string) {
 		panicIfErr(ioutil.WriteFile(rsaDir+"/known_hosts", knownhost, 0500))
 	} else {
 		defLogin := fmt.Sprintf("default login %s password %s", payload.Source.Username, payload.Source.Password)
-		panicIfErr(ioutil.WriteFile(os.ExpandEnv("$HOME/.netrc"), []byte(defLogin), 0500))
+		panicIfErr(ioutil.WriteFile(os.ExpandEnv("$HOME/.netrc"), []byte(defLogin), 0644))
 	}
+
+	panicIfErr(exec.Command("git", "clone", payload.Source.URI, destFolder).Run())
+	panicIfErr(os.Chdir(destFolder))
+
+	gitmerge := exec.Command("git", "merge", payload.Version["sha"])
+	stderr, err := gitmerge.StderrPipe()
+	panicIfErr(err)
+	panicIfErr(gitmerge.Start())
+	slurp, _ := ioutil.ReadAll(stderr)
+	if err = gitmerge.Wait(); err != nil {
+		fmt.Fprintf(os.Stderr, "merge error: %s/n", string(slurp))
+		panicIfErr(err)
+	}
+
+	return map[string]map[string]string{"version": payload.Version}
 }
 
-func check() {
+func check() []*MergeRequest {
 
 	latestVersion := payload.Version["sha"]
-
-	_ = port
-
-	gitlabAPI = fmt.Sprintf("%s://%s/api/v4/projects/%s", protocol, gitlabHost, url.PathEscape(projectPath))
-
-	if payload.Source.NoSsl {
-		protocol = "http"
-	}
-
 	lastProcessedMrCommitTs := time.Time{}
-
 	if len(latestVersion) != 0 {
 		lastProcessedMrCommitTs = getMRLastUpdate(latestVersion)
 	}
+
 	newMRs := []*MergeRequest{}
 	var openMRs []*MergeRequest
-	resp := sendrequest("GET", fmt.Sprintf("%s/merge_requests?state=opened&order_by=updated_at", gitlabAPI), "")
+	resp := sendAPIRequest("GET", "merge_requests?state=opened&order_by=updated_at", nil, nil)
 	panicIfErr(json.Unmarshal(resp, &openMRs))
 
 	for _, mr := range openMRs {
@@ -152,14 +238,11 @@ func check() {
 		newMRs = append(newMRs, &MergeRequest{Sha: latestVersion})
 	}
 
-	result, err := json.Marshal(newMRs)
-	panicIfErr(err)
-
-	fmt.Println(string(result))
+	return newMRs
 }
 
 func getMRLastUpdate(latestVersion string) time.Time {
-	resp := sendrequest("GET", fmt.Sprintf("%s/repository/commits/%s", gitlabAPI, latestVersion), "")
+	resp := sendAPIRequest("GET", "repository/commits/"+latestVersion, nil, nil)
 
 	var commit Commit
 	panicIfErr(json.Unmarshal(resp, &commit))
@@ -170,10 +253,14 @@ func getMRLastUpdate(latestVersion string) time.Time {
 	return parsed.UTC()
 }
 
-func sendrequest(method, url, body string) []byte {
+func sendAPIRequest(method, suburl string, body []byte, header map[string]string) []byte {
 
-	req, err := http.NewRequest(method, url, bytes.NewBuffer([]byte(body)))
-	for k, v := range apiRequestHeader {
+	url := gitlabAPIbase + suburl
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+
+	req.Header.Set("private-token", payload.Source.PrivateToken)
+	for k, v := range header {
 		req.Header.Set(k, v)
 	}
 
@@ -189,7 +276,7 @@ func sendrequest(method, url, body string) []byte {
 		return result
 	}
 
-	panic(fmt.Sprintf("request from %s returned status code: %d(%s)", url, resp.StatusCode, resp.Status))
+	panic(fmt.Sprintf("request sent to '%s' returned with non-success status: %s", url, resp.Status))
 }
 
 func decomposeURI() {
@@ -223,10 +310,11 @@ func configureSslVerification() {
 func panicIfErr(err error) {
 	if err != nil {
 		fpcs := make([]uintptr, 1)
+		runtime.Callers(2, fpcs)
 		caller := runtime.FuncForPC(fpcs[0] - 1)
 		callerMethod, line := caller.FileLine(fpcs[0] - 1)
 
-		fmt.Printf("\nPanic at %s : %d \n", callerMethod, line)
+		fmt.Fprintf(os.Stderr, "\nPanic at %s : line %d \n", callerMethod, line)
 
 		panic(err)
 	}
